b0d079149c7202e3ab0cbd215006daf07c247e53
vidroid16 <shalyaandrey16@gmail.com>
2019-12-09 19:26:05 +0300

    doGet redirection was returned

--- src/ControllerServlet.java
+++ src/ControllerServlet.java
@@ -16,7 +16,10 @@ public class ControllerServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        if(req.getParameterMap().size()==0){
+            System.out.println("ZAEBIS");
+            req.getRequestDispatcher("index.jsp").forward(req, resp);
+        }
     }
     //    @Override
 //    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
